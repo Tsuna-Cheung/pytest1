ee009aee44cbf61a868dfaca6640273bd09d62c4
harrycheung <237971264@qq.com>
2018-11-12 17:24:35 +0000

    Initial commit

---
+++ test1.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.style
+import matplotlib as mpl
+
+mpl.rcParams['figure.figsize'] = [2, 2] # inches
+mpl.rcParams['font.size'] = 8
+
+# Define a function to do the most common plotting task
+def xyplot(x1=None, y1=None, x2=None, y2=None, x3=None, y3=None, title=None, fname=None):
+    plt.figure()
+    if x1 is not None and y1 is not None:
+        plt.plot(x1,y1,'b.')
+    if x2 is not None and y2 is not None:
+        plt.plot(x2,y2,'k-')
+    if x3 is not None and y3 is not None:
+        plt.plot(x3,y3,'r-')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title(title)
+    plt.tight_layout()
+    if fname:
+        plt.savefig(fname)
+
+np.random.seed(123123)
+x = np.array([0, 1, 2, 3, 4, 5])
+y = 3*x+2
+# Add the noise
+t = y + 5*(np.random.rand(6)-0.5)
+xyplot(x,t,x2=x,y2=y,title=r'$y=3x+2$',fname='simple-linear.pdf')
